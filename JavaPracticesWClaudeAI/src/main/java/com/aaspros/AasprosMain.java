08d84d369f5b10976d15b60f8a9efd8481a41d4d
Alify Silva <alify.alves@hotmail.com>
2026-04-04 18:22:39 -0300

    Práticas de exercícios com Claude - Até o momento

---
+++ JavaPracticesWClaudeAI/src/main/java/com/aaspros/AasprosMain.java
@@ -0,0 +1,13 @@
+package com.aaspros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AasprosMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
